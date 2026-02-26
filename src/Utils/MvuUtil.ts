4ab361a4b4b28d032ee61812706af2be612e4cb1
ka-cat <3159409764@qq.com>
2026-02-26 09:23:49 +0800

    del: 去除多余内容

--- src/Utils/MvuUtil.ts
+++ src/Utils/MvuUtil.ts
@@ -7,8 +7,10 @@ const updateMvuDataFromContent = async ()=>{
   const mvu_data = Mvu.getMvuData({ type: 'message', message_id: -1 });
   const content = getChatMessages(-1)[0].message
   const new_data = await Mvu.parseMessage(content, mvu_data);
-
-  await Mvu.replaceMvuData(new_data, { type: 'message', message_id: getLastMessageId() });
+  if(new_data){
+    await Mvu.replaceMvuData(new_data, { type: 'message', message_id: getLastMessageId() });
+    await eventEmit(Mvu.events.VARIABLE_UPDATE_ENDED,new_data,mvu_data);
+  }
 }
 
 
@@ -20,8 +22,11 @@ const updateMvuData = async (content : string) =>{
 
   const mvu_data = Mvu.getMvuData({ type: 'message', message_id: -1 });
   const new_data = await Mvu.parseMessage(content, mvu_data);
+  if(new_data){
+    await Mvu.replaceMvuData(new_data, { type: 'message', message_id: getLastMessageId() });
+    await eventEmit(Mvu.events.VARIABLE_UPDATE_ENDED,new_data,mvu_data);
+  }
 
-  await Mvu.replaceMvuData(new_data, { type: 'message', message_id: getLastMessageId() });
 }
 
 /**
@@ -29,16 +34,31 @@ const updateMvuData = async (content : string) =>{
  */
 const backUpMvuData = async ()=>{
   await waitGlobalInitialized('Mvu');
-
+  const old_data = Mvu.getMvuData({ type: 'message', message_id: -1 });
   const mvu_data = Mvu.getMvuData({ type: 'message', message_id: Math.max(0,getLastMessageId()-1) });
   if(mvu_data){
     await Mvu.replaceMvuData(mvu_data, { type: 'message', message_id: getLastMessageId() });
+    await eventEmit(Mvu.events.VARIABLE_UPDATE_ENDED,mvu_data,old_data);
+  }
+}
+
+/**
+ * 通过变量对象覆盖更新Mvu变量
+ */
+const updateMvuDataByObj = async(obj: object) => {
+  const mvuData = Mvu.getMvuData({ type: 'message', message_id: -1 });
+  const newData = mvuData;
+  newData.stat_data = obj;
+  if(newData){
+    await Mvu.replaceMvuData(newData, { type: 'message', message_id: getLastMessageId() });
+    await eventEmit(Mvu.events.VARIABLE_UPDATE_ENDED,newData,mvuData);
   }
 }
 
 export const MvuUtil = {
   updateMvuDataFromContent,
   updateMvuData,
-  backUpMvuData
+  backUpMvuData,
+  updateMvuDataByObj
 }
 
